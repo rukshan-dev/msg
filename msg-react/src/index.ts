722f4e50b85932ee41992d13be49c297a490cc32
Rukshan <ruk.fernando93@gmail.com>
2025-04-27 15:46:08 +0200

    feat(react): added react connector

---
+++ msg-react/src/index.ts
@@ -0,0 +1,34 @@
+import {
+  getSelectorPath,
+  isPathImpacted,
+  StoreReturn,
+} from "@repacked-tools/msg";
+import { useRef, useSyncExternalStore } from "react";
+
+type AnyObject = Record<string | symbol | number, any>;
+
+type Selector<T extends AnyObject, K = any> = (state: T) => K;
+
+export const useSelector = <T extends AnyObject, K>(
+  store: StoreReturn<T, any>,
+  selector: Selector<T, K>
+) => {
+  const immutableRef = useRef<any>({ value: null });
+  useSyncExternalStore(
+    (callback) => {
+      const subscribe = store.subscribe((detail) => {
+        const selectedPath = getSelectorPath(selector);
+        if (isPathImpacted(detail.paths, selectedPath)) {
+          immutableRef.current = { value: selector(store.getState()) };
+          callback();
+        }
+      });
+      return subscribe.unsubscribe;
+    },
+    () => {
+      return immutableRef.current;
+    }
+  );
+
+  return selector(store.getState());
+};
